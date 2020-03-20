36f22ebb9b844458adb6e799ebdd7d8d6cc547b0
hookxfocus <61572324+hookxfocus@users.noreply.github.com>
2020-03-20 17:24:34 +0800

    Add files via upload

---
+++ firstOpcv.h
@@ -0,0 +1,32 @@
+﻿
+// firstOpcv.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CfirstOpcvApp:
+// 有关此类的实现，请参阅 firstOpcv.cpp
+//
+
+class CfirstOpcvApp : public CWinApp
+{
+public:
+	CfirstOpcvApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CfirstOpcvApp theApp;
